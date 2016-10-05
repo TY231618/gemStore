6b80da6dc66b5f708fbfaab7496b88339e8624e9
tonious <tony.young@capco.com>
2016-10-05 18:50:25 +0100

    added ng-hide if product sold out

--- app.js
+++ app.js
@@ -9,7 +9,8 @@
   var gem = {
     name: 'Dodecahedron',
     price: 2.95,
-    description: '...',
+    description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
     canPurchase: true,
+    soldOut: false
   }
 })();
