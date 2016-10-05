01c6e9737181f5d94dfc59140c70a8eea2713f35
tonious <tony.young@capco.com>
2016-10-05 18:58:22 +0100

    added more gems and ng-repeat to list all

--- app.js
+++ app.js
@@ -3,14 +3,37 @@
   var app = angular.module('store', []);
 
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-    canPurchase: true,
-    soldOut: false
-  }
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: true,
+      soldOut: false
+    },
+    {
+      name: 'Ruby',
+      price: 99.99,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: true,
+      soldOut: false
+    },
+    {
+      name: 'Emerald',
+      price: 59.99,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: false,
+      soldOut: false
+    },
+    {
+      name: 'Diamond',
+      price: 119.95,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: true,
+      soldOut: false
+    }
+  ]
 })();
