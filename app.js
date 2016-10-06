679305986f3e29fb0fc07fbdbcbfc2a90295d77b
tonious <tony.young@capco.com>
2016-10-06 14:38:42 +0100

    added images to object and index with ng-src

--- app.js
+++ app.js
@@ -12,28 +12,52 @@
       price: 2.95,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        {
+          full: 'img/Dodecahedron300.jpg',
+          thumb: 'img/DodecahedronThumb.jpg',
+        }
+      ]
     },
     {
       name: 'Ruby',
       price: 99.99,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        {
+          full: 'img/ruby300.jpg',
+          thumb: 'img/rubyThumb.jpg',
+        }
+      ]
     },
     {
       name: 'Emerald',
       price: 59.99,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
       canPurchase: false,
-      soldOut: false
+      soldOut: false,
+      images: [
+        {
+          full: 'img/emerald300.jpg',
+          thumb: 'img/emeraldThumb.jpg',
+        }
+      ]
     },
     {
       name: 'Diamond',
       price: 119.95,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        {
+          full: 'img/diamond300.jpg',
+          thumb: 'img/diamondThumb.jpg',
+        }
+      ]
     }
   ]
 })();
