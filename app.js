96dccb2c2338a662a546ec9475d1ba7d286c3f1b
tonious <tony.young@capco.com>
2016-10-08 15:26:49 +0100

    trying to add  service for calling in gems.json

--- app.js
+++ app.js
@@ -2,142 +2,18 @@
 (function() {
   var app = angular.module('store', ['store-reviews', 'store-products']);
 
-  app.controller('StoreController', function() {
-    this.products = gems;
-  });
+  app.controller('StoreController', ['$http', function($http) {
+    // this.products = gems;
+    var store = this;
+    store.gems = [];
 
-  var gems = [
-    {
-      name: 'Dodecahedron',
-      price: 2.95,
-      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-      canPurchase: true,
-      soldOut: false,
-      images: [
-        {
-          full: 'img/Dodecahedron300.jpg',
-          thumb: 'img/DodecahedronThumb.jpg',
-        }
-      ],
-      reviews: [
-        {
-          stars: '4',
-          comment: 'Awesome product',
-          email: 'doe@mail.com',
-          createdOn: 1397490980837
-        },
-        {
-          stars: '1',
-          comment: 'Terrible product',
-          email: 'jane@mail.com',
-          createdOn: 1397490980837
-        },
-        {
-          stars: '5',
-          comment: 'Perfect product',
-          email: 'Mike@mail.com',
-          createdOn: 1397490980837
-        }
-      ]
-    },
-    {
-      name: 'Ruby',
-      price: 99.99,
-      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-      canPurchase: true,
-      soldOut: false,
-      images: [
-        {
-          full: 'img/ruby300.jpg',
-          thumb: 'img/rubyThumb.jpg',
-        }
-      ],
-      reviews: [
-        {
-          stars: '4',
-          comment: 'Awesome product',
-          email: 'doe@mail.com',
-          createdOn: 1397490980837
-        },
-        {
-          stars: '1',
-          comment: 'Terrible product',
-          email: 'jane@mail.com',
-          createdOn: 1397490980837
-        },
-        {
-          stars: '5',
-          comment: 'Perfect product',
-          email: 'Mike@mail.com',
-          createdOn: 1397490980837
-        }
-      ]
-    },
-    {
-      name: 'Emerald',
-      price: 59.99,
-      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-      canPurchase: false,
-      soldOut: false,
-      images: [
-        {
-          full: 'img/emerald300.jpg',
-          thumb: 'img/emeraldThumb.jpg',
-        }
-      ],
-      reviews: [
-        {
-          stars: '4',
-          comment: 'Awesome product',
-          email: 'doe@mail.com',
-          createdOn: 1397490980837
-        },
-        {
-          stars: '1',
-          comment: 'Terrible product',
-          email: 'jane@mail.com',
-          createdOn: 1397490980837
-        },
-        {
-          stars: '5',
-          comment: 'Perfect product',
-          email: 'Mike@mail.com',
-          createdOn: 1397490980837
-        }
-      ]
-    },
-    {
-      name: 'Diamond',
-      price: 119.95,
-      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-      canPurchase: true,
-      soldOut: false,
-      images: [
-        {
-          full: 'img/diamond300.jpg',
-          thumb: 'img/diamondThumb.jpg',
-        }
-      ],
-      reviews: [
-        {
-          stars: '4',
-          comment: 'Awesome product',
-          email: 'doe@mail.com',
-          createdOn: 1397490980837
-        },
-        {
-          stars: '1',
-          comment: 'Terrible product',
-          email: 'jane@mail.com',
-          createdOn: 1397490980837
-        },
-        {
-          stars: '5',
-          comment: 'Perfect product',
-          email: 'Mike@mail.com',
-          createdOn: 1397490980837
-        }
-      ]
-    }
-  ]
+    $http.get('/gems.json').success(function(data){
+      store.gems = data;
+    });
+
+  }]);
+
+  // var gems = [
+  //
+  // ]
 })();
