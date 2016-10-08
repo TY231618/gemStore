bbaf3d71a1b6e9dcc1fa6fc2b5bf52e1cc74a08c
tonious <tony.young@capco.com>
2016-10-08 14:31:13 +0100

    removed PanelController completely and put its functionality into directive, removed ng-controller from panel-template.html

--- app.js
+++ app.js
@@ -6,19 +6,6 @@
     this.products = gems;
   });
 
-  app.controller('PanelController', function() {
-    this.tab = 1;
-
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
-
-    this.isSelected = function(checkTab) {
-      return this.tab === checkTab;
-    };
-
-  });
-
   app.controller('ReviewController', function() {
     this.review = {};
 
@@ -49,7 +36,17 @@
     return {
       restrict: 'E',
       templateUrl: 'panel-template.html',
-      controller: function() {},
+      controller: function() {
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
       controllerAs: 'panel'
     }
   });
